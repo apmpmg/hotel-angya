34ef7ed0a0828d4b89a63506a85812e17db4bd72
apmpmg <morikawatakuma482@gmail.com>
2026-05-28 04:41:56 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 function showPage(page){
 
-  const content = document.getElementById("content");
+const content = document.getElementById("content");
 
-  if(page === "home"){
-    content.innerHTML = "ホーム画面";
-  }
+if(page === "home"){
+content.innerHTML = "<div class='card'>ホーム画面</div>";
+}
 
-  if(page === "search"){
-    content.innerHTML = "検索画面";
-  }
+if(page === "search"){
+content.innerHTML = `
+<div class='card'>
+<h2>ホテル検索</h2>
+<input placeholder='ホテル名検索'>
+</div>
 
-  if(page === "record"){
-    content.innerHTML = "行脚記録画面";
-  }
+<div class='card'>
+<h3>ホテルA</h3>
+<p>新宿</p>
+</div>
 
-  if(page === "ranking"){
-    content.innerHTML = "ランキング画面";
-  }
+<div class='card'>
+<h3>ホテルB</h3>
+<p>歌舞伎町</p>
+</div>
+`;
+}
 
-  if(page === "mypage"){
-    content.innerHTML = "マイページ";
-  }
+if(page === "record"){
+content.innerHTML = "<div class='card'>行脚記録画面</div>";
+}
+
+if(page === "mypage"){
+content.innerHTML = "<div class='card'>マイページ</div>";
+}
 
 }
+
+showPage('home');
