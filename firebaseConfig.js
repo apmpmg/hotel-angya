69a115a1de161d5318d3ba342b4b80a4a91333b8
apmpmg <morikawatakuma482@gmail.com>
2026-05-28 08:24:40 +0900

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNI-soevLMrw6agr8zCVRdme5eVniMdP4",
+  authDomain: "hotel-pilgrimage.firebaseapp.com",
+  projectId: "hotel-pilgrimage",
+  storageBucket: "hotel-pilgrimage.firebasestorage.app",
+  messagingSenderId: "260966152115",
+  appId: "1:260966152115:web:6a6ca1254e87eb7202bdc6",
+  measurementId: "G-PDKKBKWCCN"
+};
