6810ffb5f7b687aa08a09261b613957f3fba020b
apmpmg <morikawatakuma482@gmail.com>
2026-05-28 09:13:37 +0900

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCNI-soevLMrw6agr8zCVRdme5eVniMdP4",
   authDomain: "hotel-pilgrimage.firebaseapp.com",
   projectId: "hotel-pilgrimage",
